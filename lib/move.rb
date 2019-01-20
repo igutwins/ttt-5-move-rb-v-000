a8bd9fe4033e167384ce15a31cde3f180ce7916c
Ian Gutwinski <iangutwinski@gmail.com>
2019-01-20 00:33:22 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,7 +9,7 @@ end
 # code your input_to_index and move method here!
 #takes a user's input and convert to index of the board array
 def input_to_index(position)
-  position.to_i
+  position.to_i - 1
 end
 
 #move takes the index and translates to a cell
